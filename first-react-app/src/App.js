8a5b6e02c465e0f203cc74a368ccf2a816ca415c
Vineet Brahmankar <vinitb88@gmail.com>
2019-07-10 20:11:13 +0530

    Functional Component

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,56 +1,102 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Persons from './Components/Persons/Persons'
 
-class App extends Component {
-state =  { 
-  persons : [
-  {
-    name : 'Vineet',
-  age : '29'
-  },
-  {
-    name :  'Rakhi',
-    age : '23'
-  },
-  {
-    name :  'Savita',
-    age : '26'
-  }
-  ]  
-}
+//class App extends Component {
+// state =  
+// { 
+//   persons : [
+//     {
+//     name : 'Vineet',
+//     age : '29'
+//     },
+//     {
+//     name :  'Rakhi',
+//     age : '23'
+//     },
+//     {
+//     name :  'Savita',
+//     age : '26'
+//     }
+//   ],
+//   otherState : 'Other State' 
+// }
 
-switchHandler = () =>
-{
+
+// switchHandler = () =>
+// {
+//   //console.log("was clicked")
+//   this.setState({
+//     persons : [
+//       {
+//         name : 'Vineet',
+//       age : '29'
+//       },
+//       { 
+//         name :  'Maxx',
+//         age : '23'
+//       },
+//       {
+//         name :  'Savita',
+//         age : '31'
+//       }
+//       ] 
+//   })
+//   console.log(this.state)
+// }
+  //render() {
+
+  const App = (props) => {
+    const [newArr,newSetState] = useState ( 
+    {
+      newPersons : [
+        {
+          name : 'Vineet',
+        age : '29'
+        },
+        { 
+          name :  'Maxx',
+          age : '23'
+        },
+        {
+          name :  'Savita',
+          age : '31'
+        }
+        ]
+    })
+
+    const switchHandler = () =>
+    {
   //console.log("was clicked")
-  this.setState({
-    persons : [
+  newSetState({
+    newPersons : [
       {
         name : 'Vineet',
-      age : '29'
+      age : '35'
       },
-      {
+      { 
         name :  'Maxx',
         age : '23'
       },
       {
-        name :  'Savita',
-        age : '31'
+        name :  'Chris',
+        age : '40'
       }
       ] 
   })
+  console.log(newArr)
 }
-  render() {
+
     return (
       <div className="App">
         <h1>Hi I am a React App</h1>
-        <button onClick={this.switchHandler}>Switch Name</button>
-        <Persons  name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Persons  name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobby is Playing Chess </Persons>
-        <Persons name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={switchHandler}>Switch Name</button>
+        <Persons  name={newArr.newPersons[0].name} age={newArr.newPersons[0].age}/>
+        <Persons  name={newArr.newPersons[1].name} age={newArr.newPersons[1].age}>My Hobby is Playing Chess </Persons>
+        <Persons name={newArr.newPersons[2].name} age={newArr.newPersons[2].age}/>
       </div>
     );
   }
-}
+ // }
 
 export default App;
